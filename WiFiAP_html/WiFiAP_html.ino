2acba462b494a2aa15b08008f43083c5fa56abf3
Joseph Voss <josephvoss14@gmail.com>
2017-10-29 21:02:32 -0500

    Adding async control

--- WiFiAP_html/WiFiAP_html.ino
+++ WiFiAP_html/WiFiAP_html.ino
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2015, Majenko Technologies
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * 
- * * Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- * 
- * * Neither the name of Majenko Technologies nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /* Create a WiFi access point and provide a web server on it. */
 
 #include <ESP8266WiFi.h>
@@ -36,13 +6,66 @@
 //#include <ESP8266WiFiMulti.h> 
 #include <ESP8266WebServer.h>
 #include <Hash.h>
+#include <TimerObject.h>
 
 #include "html_file.h"
-
 
 /* Set these to your desired credentials. */
 const char *ssid = "ESPap";
 const char *password = "thereisnospoon";
+
+/* Pin definition */
+const int LED_PIN = 2;//LED_BUILTIN;
+
+// Callback timer function
+TimerObject *timer10ms = new TimerObject(100); //will call the callback in the interval of 100 ms
+
+// Data buffer objects
+int rand_data[64+3];
+char rand_string[300];
+
+#define LENGTH_ASYNC    1   // Number of Async pins
+// Pins to drive async
+int async_pins[LENGTH_ASYNC] = {LED_PIN};
+// State of those pins
+int async_state[LENGTH_ASYNC];
+// Time for those pins to be in that state
+int async_timeoff[LENGTH_ASYNC];
+
+void asynclikeDrive(int desired_state[LENGTH_ASYNC], int desired_time[LENGTH_ASYNC])
+/*
+    Drive a pin asynchronously. Call to set the status of a pin
+*/
+{
+    unsigned long curr_time = millis();
+    // Loop over all pins
+    for (int i=0; i < LENGTH_ASYNC; i++)
+    {
+        // Set new state and set timer
+        async_state[i] = desired_state[i];
+        async_timeoff[i] = desired_time[i] + curr_time;
+        digitalWrite(async_pins[i], async_state[i]);
+    }
+}
+
+void asynclikeUpdate()
+/*
+    Update the asynchronous driven pins.
+*/
+{
+    unsigned long curr_time = millis();
+    // Loop over all pins
+    for (int i=0; i < LENGTH_ASYNC; i++)
+    {
+        // Check state and check timer
+        if (async_state[i] == 0 && curr_time >= async_timeoff[i])
+        {
+            async_state[i] = 1;
+            digitalWrite(async_pins[i], async_state[i]);
+        }
+    }
+
+}
 
 ESP8266WebServer server(80);
 /* Just a little test message.  Go to http://192.168.4.1 in a web browser
@@ -57,40 +80,75 @@ void webSocketHandler(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
 
     switch(type) {
         case WStype_DISCONNECTED:
-            Serial.printf("[%u] Disconnected!\n", num);
+            {
+                Serial.printf("[%u] Disconnected!\n", num);
+            }
             break;
         case WStype_CONNECTED:
             {
                 IPAddress ip = webSocket.remoteIP(num);
                 Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-				
-				// send message to client
-//				webSocket.sendTXT(num, "Connected");
+                int led_state[1] = {0};
+                int led_time[1] = {10};
+                asynclikeDrive(led_state, led_time); 
             }
             break;
         case WStype_TEXT:
-            Serial.printf("[%u] get Text: %s\n", num, payload);
-
-            // send message to client
-            // webSocket.sendTXT(num, "message here");
-
-            // send data to all connected clients
-            // webSocket.broadcastTXT("message here");
+            {
+                Serial.printf("[%u] get Text: %s\n", num, payload);
+                int led_state[1] = {0};
+                int led_time[1] = {100};
+                asynclikeDrive(led_state, led_time); 
+            }
             break;
         case WStype_BIN:
-            Serial.printf("[%u] get binary length: %u\n", num, length);
-            hexdump(payload, length);
-
-            // send message to client
-            // webSocket.sendBIN(num, payload, length);
+            {
+                Serial.printf("[%u] get binary length: %u\n", num, length);
+                hexdump(payload, length);
+            }
             break;
     }
 
 }
 
+void SampleData() {
+/*  
+    Eventually will make calls to read thermal camera and other sensors, but for
+    now this function is used purely to generate random data to broadcast to 
+    clients
+*/
+   
+    int length = 0;
+
+    for (int i=0;i<64+3;i++) {
+        if (i == 0) 
+        {
+            rand_data[i] = 1;
+        }
+        else
+        {
+            rand_data[i] = random(99);
+        }
+        length += sprintf(rand_string+length, "%d,", rand_data[i]);
+    }
+
+    webSocket.broadcastTXT(rand_string);
+}
+
 void setup() {
-	delay(1000);
-	Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN,LOW);
+	delay(250);
+    digitalWrite(LED_PIN,HIGH);
+	delay(250);
+    digitalWrite(LED_PIN,LOW);
+	delay(250);
+    digitalWrite(LED_PIN,HIGH);
+	delay(250);
+    digitalWrite(LED_PIN,LOW);
+	delay(250);
+    digitalWrite(LED_PIN,HIGH);
+	Serial.begin(9600);
 	Serial.println();
 	Serial.print("Configuring access point...");
 	/* You can remove the password parameter if you want the AP to be open. */
@@ -99,30 +157,22 @@ void setup() {
 	IPAddress myIP = WiFi.softAPIP();
 	Serial.print("AP IP address: ");
 	Serial.println(myIP);
+    
+    webSocket.begin();
+    webSocket.onEvent(webSocketHandler);
+	Serial.println("Websocket Started");
 
 	server.on("/", handleRoot);
 	server.begin();
 	Serial.println("HTTP server started");
 
-    webSocket.begin();
-    webSocket.onEvent(webSocketHandler);
-	Serial.println("Websocket Started");
+    timer10ms->setOnTimer(&SampleData);
+    timer10ms->Start(); //start the thread.
 }
 
 void loop() {
-	Serial.println("Loopin");
 	server.handleClient();
     webSocket.loop();
-
-    int rand_data[64+3];
-    char rand_string[300];
-    int length = 0;
-
-    for (int i=0;i<64+3;i++) {
-        rand_data[i] = random(99);
-        length += sprintf(rand_string+length, "%d,", rand_data[i]);
-    }
-
-    webSocket.broadcastTXT(rand_string);
-    delay(1000);
+    timer10ms->Update();
+    asynclikeUpdate();
 }
